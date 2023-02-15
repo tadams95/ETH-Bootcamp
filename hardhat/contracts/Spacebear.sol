3a1d773eb83bdd70d9351694cea75c57b313d0d2
tadams95 <tyrelleadams28@gmail.com>
2023-02-15 15:36:59 -0800

    Unit Testing using HardHat and Mocha

--- hardhat/contracts/Spacebear.sol
+++ hardhat/contracts/Spacebear.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
 contract Spacebear is ERC721, Ownable {
     using Counters for Counters.Counter;
 
