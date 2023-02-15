0fd885746c99238693c4d27ccfa6b07a25719a0d
tadams95 <tyrelleadams28@gmail.com>
2023-02-15 14:33:08 -0800

    Intro to HardHat and Configure

--- NFT-Project:Truffle/contracts/SpaceBear.sol
+++ NFT-Project:Truffle/contracts/SpaceBear.sol
@@ -1,41 +1,49 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@ganache/console.log/console.sol";
 
-contract SpaceBear is ERC721, ERC721URIStorage, Ownable {
+contract Spacebear is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("SpaceBear", "SBR") {}
+    constructor() ERC721("Spacebear", "SBR") {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
+        return
+            "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    }
+
+    function buyToken() public payable {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(msg.value == tokenId * 0.1 ether, "Not enough funds sent");
+        console.log("got here", tokenId, msg.value);
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override(ERC721) {
         super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
         public
-        view
-        override(ERC721, ERC721URIStorage)
+        pure
+        override(ERC721)
         returns (string memory)
     {
-        return super.tokenURI(tokenId);
+        return string(abi.encodePacked(_baseURI(), "_", tokenId + 1, ".json"));
     }
 }
