f6f231ecf49f2f23d30f1398f1d5e01e92566c54
tadams95 <tyrelleadams28@gmail.com>
2023-02-17 15:58:29 -0800

    Prevent Double Spend

--- SharedWallet/SimpleWallet.sol
+++ SharedWallet/SimpleWallet.sol
@@ -22,12 +22,18 @@ contract SimpleWallet is Ownable {
         _;
     }
 
-    function withdrawMoney(address payable _to, uint256 _amount)
-        public
-        ownerOrAllowed(_amount)
-    {
+   function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount) {
+        allowance[_who] -= _amount;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "Contract doesn't own enough money");
+        if(!isOwner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
         _to.transfer(_amount);
     }
+
 
     receive()  external payable  {
 
