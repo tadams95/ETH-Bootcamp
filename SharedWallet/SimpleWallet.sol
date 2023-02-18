7ee5d40ad4cf64bf582fea3f31d9ff5b0317af2d
tadams95 <tyrelleadams28@gmail.com>
2023-02-17 16:05:30 -0800

    Improve Smart Contract (SC) readability

--- SharedWallet/SimpleWallet.sol
+++ SharedWallet/SimpleWallet.sol
@@ -4,38 +4,47 @@ pragma solidity 0.8.17;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SimpleWallet is Ownable {
-   
-    mapping(address => uint) public allowance;
+contract Allowance is Ownable {
+    mapping(address => uint256) public allowance;
 
-    function addAllowance(address _who, uint _amount) public onlyOwner {
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
         allowance[_who] = _amount;
     }
 
-    function isOwner() internal view returns(bool) {
+    function isOwner() internal view returns (bool) {
         return owner() == msg.sender;
     }
-  
 
-    modifier ownerOrAllowed(uint _amount) {
-        require(isOwner() || allowance[msg.sender] > _amount, "You are not allowed");
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(
+            isOwner() || allowance[msg.sender] > _amount,
+            "You are not allowed"
+        );
         _;
     }
 
-   function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount) {
+    function reduceAllowance(address _who, uint256 _amount)
+        internal
+        ownerOrAllowed(_amount)
+    {
         allowance[_who] -= _amount;
     }
+}
 
-    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
-        require(_amount <= address(this).balance, "Contract doesn't own enough money");
-        if(!isOwner()) {
+contract SimpleWallet is Allowance {
+    function withdrawMoney(address payable _to, uint256 _amount)
+        public
+        ownerOrAllowed(_amount)
+    {
+        require(
+            _amount <= address(this).balance,
+            "Contract doesn't own enough money"
+        );
+        if (!isOwner()) {
             reduceAllowance(msg.sender, _amount);
         }
         _to.transfer(_amount);
     }
 
-
-    receive()  external payable  {
-
-    }
+    receive() external payable {}
 }
