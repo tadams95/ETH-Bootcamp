7a9d55b3f70e5b0c919c8b1344dda8a088e2fa0d
tadams95 <tyrelleadams28@gmail.com>
2023-02-19 18:20:35 -0800

    Refactor Code for UX

--- SupplyChain/itemManager.sol
+++ SupplyChain/itemManager.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+//responsible for taking payment then transfer payment completion to ItemManager
+contract Item {
+    uint public priceInWei;
+    uint public index;
+    uint public pricePaid;
+
+    ItemManager parentContract;
+
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
+         priceInWei = _priceInWei;
+        index = _index;
+
+        parentContract = _parentContract;
+    }
+
+    receive() external payable {
+        require(pricePaid == 0, "Item is paid already");
+        require(priceInWei == msg.value, "Only full payments allowed");
+        pricePaid += msg.value;
+        (bool success, ) = address(parentContract).call{value: msg.value}(abi.encodeWithSignature("triggerPayment(uint256)", index));
+        require(success, "The transaction wasn't successful, cancelling");
+    }
+}
+
 contract ItemManager {
     enum SupplyChainState {
         Created,
@@ -10,6 +34,7 @@ contract ItemManager {
 
     // a struct is a way to create a new data type
     struct S_Item {
+        Item _item;
         string _identifier;
         uint256 _itemPrice;
         ItemManager.SupplyChainState _state;
@@ -22,6 +47,8 @@ contract ItemManager {
     event SupplyChainStep(uint256 _itemIndex, uint256 _step);
 
     function createItem(string memory _identifier, uint256 _itemPrice) public {
+        Item item = new Item(this, _itemPrice, itemIndex);
+        items[itemIndex]._item = item;
         items[itemIndex]._identifier = _identifier;
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
